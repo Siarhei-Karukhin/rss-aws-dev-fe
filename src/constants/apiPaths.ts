16fce1aec9f9f9315a5d2c5a6f1076a0133ef073
Siarhei Karukhin <Siarhei_Karukhin@epam.com>
2024-08-01 02:47:37 +0300

    feat: connect Elastic Beanstalk api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,8 +2,8 @@ const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://7r054419yd.execute-api.us-west-2.amazonaws.com/prod",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "http://siarhei-karukhin-bff-api-develop.us-west-2.elasticbeanstalk.com/product-api",
+  cart: "http://siarhei-karukhin-bff-api-develop.us-west-2.elasticbeanstalk.com/cart-api",
 };
 
 export default API_PATHS;
