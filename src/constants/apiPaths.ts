ef8d07abeba887775ecc9480f166038a370d6803
Siarhei Karukhin <Siarhei_Karukhin@epam.com>
2024-08-01 15:21:24 +0300

    fix: rollback API_PATHS cart

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://7r054419yd.execute-api.us-west-2.amazonaws.com/prod",
   bff: "http://siarhei-karukhin-bff-api-develop.us-west-2.elasticbeanstalk.com/product-api",
-  cart: "http://siarhei-karukhin-bff-api-develop.us-west-2.elasticbeanstalk.com/cart-api",
+  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
